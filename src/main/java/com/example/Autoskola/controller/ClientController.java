1c914de59d1717e83888618c2a30717a596bc446
22DP1AKaha <131352892+22DP1AKaha@users.noreply.github.com>
2024-04-26 08:33:54 +0000

    pielabojumi

--- src/main/java/com/example/Autoskola/controller/ClientController.java
+++ src/main/java/com/example/Autoskola/controller/ClientController.java
@@ -3,11 +3,9 @@ package com.example.Autoskola.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 
 import com.example.Autoskola.entity.Client;
 import com.example.Autoskola.repository.ClientRepository;
-import java.util.List;
 
 
 
@@ -18,13 +16,7 @@ public class ClientController {
     private ClientRepository clientRepository;
     
 
-    @GetMapping("/profile")
-    public String aboutProfile(Model model) {
-        List<Client> clients = clientRepository.findAll();
-        model.addAttribute("clients", clients);
-        return "profile";
 
-    }
     public String showProfile(Model model) {
         String loggedInUsername = "testuser";
 
