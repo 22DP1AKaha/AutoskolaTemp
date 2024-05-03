3caf4e2d799c50071039e5fb018905172c672f20
22DP1AKaha <131352892+22DP1AKaha@users.noreply.github.com>
2024-05-03 15:07:22 +0000

    pabeidzu profile page

--- src/main/java/com/example/Autoskola/controller/ProfileController.java
+++ src/main/java/com/example/Autoskola/controller/ProfileController.java
@@ -4,6 +4,9 @@ package com.example.Autoskola.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
 import com.example.Autoskola.entity.Client;
 import com.example.Autoskola.repository.ClientRepository;
 import org.springframework.ui.Model;
@@ -35,6 +38,32 @@ public class ProfileController {
             return "edit_profile"; 
         }
         return "redirect:/"; 
+    }
+
+     @PostMapping("/updateProfile")
+    public String updateProfile(@ModelAttribute Client updatedClient) {
+        Client client = clientRepository.findByIsActiveTrue();
+        if (client != null) {
+            client.setName(updatedClient.getName());
+            client.setSurname(updatedClient.getSurname());
+            client.setAge(updatedClient.getAge());
+            client.setUsername(updatedClient.getUsername());
+            client.setPersonCode(updatedClient.getPersonCode());
+            if (updatedClient.getPassword() != null && !updatedClient.getPassword().isEmpty()) {
+                client.setPassword(updatedClient.getPassword());
+            }
+            clientRepository.save(client);
+        }
+        return "redirect:/profils";
+    }
+
+    @GetMapping("/DzestProfilu")
+    public String deleteProfile() {
+        Client client = clientRepository.findByIsActiveTrue();
+        if (client != null) {
+            clientRepository.delete(client); // Deleting the client from the database
+        }
+        return "redirect:/"; // Redirecting the user to the starting page
     }
 }
 
