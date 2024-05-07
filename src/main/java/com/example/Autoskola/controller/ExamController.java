eda4f4e51fb913e10b37e909355eb6adf2dbdcce
22DP1AKaha <131352892+22DP1AKaha@users.noreply.github.com>
2024-05-07 09:48:51 +0000

    pabeigts exam

--- src/main/java/com/example/Autoskola/controller/ExamController.java
+++ src/main/java/com/example/Autoskola/controller/ExamController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -24,35 +25,41 @@ public class ExamController {
 
     private final int totalQuestions = 20;
     private int currentQuestionIndex = 0;
-    private int score = 0;
+    public int score = 0;
+    private Boolean StartNewExam = true;
 
     @GetMapping("/autotests")
     public String showExamPage(Model model) {
         Client client = clientRepository.findByIsActiveTrue();
         if (client != null) {
-            List<Exam> examQuestions = examRepository.findAll();
+            if (StartNewExam = true){
+                StartNewExam = false; 
+                List<Exam> examQuestions = examRepository.findAll();
 
-            if (examQuestions.size() < totalQuestions) {
-                model.addAttribute("error", "Nav pietiekami jautājumu datubāzē.");
-                return "error_page";
+                if (examQuestions.size() < totalQuestions) {
+                    model.addAttribute("error", "Nav pietiekami jautājumu datubāzē.");
+                    return "error_page";
+                }
+
+                if (currentQuestionIndex >= totalQuestions) {
+                    return "redirect:/testarezultats";
+                }
+
+                Exam currentQuestion = examQuestions.get(currentQuestionIndex);
+                model.addAttribute("questionNumber", currentQuestionIndex + 1);
+                model.addAttribute("question", currentQuestion);
+                return "exam";
             }
-
-            if (currentQuestionIndex >= totalQuestions) {
-                return "redirect:/testarezultats";
-            }
-
-            Exam currentQuestion = examQuestions.get(currentQuestionIndex);
-            model.addAttribute("questionNumber", currentQuestionIndex + 1);
-            model.addAttribute("question", currentQuestion);
-            return "exam";
+            return "redirect:/";          
         }
         return "redirect:/";
     }
 
     @PostMapping("/submit")
-    public String submitAnswer(@RequestParam("selectedAnswer") String selectedAnswer) {
+    public String submitAnswer(@RequestParam("selectedAnswer") String selectedAnswer, Model model, RedirectAttributes redirectAttributes) {
         if (selectedAnswer == null || selectedAnswer.isEmpty()) {
-            return "redirect:/autotests?error=izvēlieties atbildi";
+            redirectAttributes.addFlashAttribute("error", "Izvēlieties atbildi"); 
+            return "redirect:/autotests";
         }
 
         List<Exam> examQuestions = examRepository.findAll();
@@ -72,10 +79,18 @@ public class ExamController {
     public String showResultPage(Model model) {
         model.addAttribute("score", score);
         if (score >= 18) {
-            model.addAttribute("result", "Congratulations! You passed the exam.");
+            model.addAttribute("result", "Apsveicam! Jūs nokārtojāt eksāmenu.");
         } else {
-            model.addAttribute("result", "Sorry, you failed the exam.");
+            model.addAttribute("result", "Diemžēl, rezultāt nav pietiekams.");
         }
         return "exam_result";
+    }
+
+    @GetMapping("/atkartotTestu")
+    public String restartExam(){
+        StartNewExam = true;
+        currentQuestionIndex = 0;
+        score = 0;
+        return "redirect:/autotests";
     }
 }
